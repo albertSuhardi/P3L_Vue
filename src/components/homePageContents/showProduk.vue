74ac8f8858cb8a960f32cac6162ba10dd205c86f
= <=>
2020-04-12 13:37:31 +0700

    last sprint 1

--- src/components/homePageContents/showProduk.vue
+++ src/components/homePageContents/showProduk.vue
@@ -1,40 +1,147 @@
 <template>
   <v-container fluid>
-    <v-data-iterator :items="shows" :search="search" hide-default-footer >
+    <v-data-iterator
+      :items="items"
+      :items-per-page.sync="itemsPerPage"
+      :page="page"
+      :search="search"
+      :sort-by="sortBy.toLowerCase()"
+      :sort-desc="sortDesc"
+      hide-default-footer
+    >
       <template v-slot:header>
-        <v-toolbar class="mb-2" color="white" flat >
-          <v-toolbar-title>List Produk</v-toolbar-title>
-          <v-text-field class="ml-12" v-model="search" append-icon="mdi-search-web" label="Search" single-line hide-details></v-text-field>
+        <v-toolbar
+          dark
+          color="#B7472A"
+          class="mb-1"
+        >
+          <v-text-field
+            v-model="search"
+            clearable
+            flat
+            solo-inverted
+            hide-details
+            label="Search"
+          ></v-text-field>
+          <template v-if="$vuetify.breakpoint.mdAndUp">
+            <v-spacer></v-spacer>
+            <v-select
+              v-model="sortBy"
+              flat
+              solo-inverted
+              hide-details
+              :items="keys"
+              label="Sort by"
+            ></v-select>
+            <v-spacer></v-spacer>
+            <v-btn-toggle
+              v-model="sortDesc"
+              mandatory
+            >
+              <v-btn
+                large
+                depressed
+                color="#B7472A"
+                :value="false"
+              >
+                <v-icon>mdi-arrow-up</v-icon>
+              </v-btn>
+              <v-btn
+                large
+                depressed
+                color="#B7472A"
+                :value="true"
+              >
+                <v-icon>mdi-arrow-down</v-icon>
+              </v-btn>
+            </v-btn-toggle>
+          </template>
         </v-toolbar>
       </template>
+
       <template v-slot:default="props">
         <v-row>
-            <v-col v-for="item in props.items" :key="item.title" cols="12" sm="6" md="4" lg="3">
-                <v-card>
-                    <v-img class="white--text align-end" height="200px" v-bind:src="'http://localhost:8081/API_REST/upload/produk/' + item.foto"></v-img>
+          <v-col
+            v-for="item in props.items"
+            :key="item.nama"
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+            <v-card>
+              <v-card-title class="subheading font-weight-bold">{{ item.nama }}</v-card-title>
 
-                    <v-divider></v-divider>
+              <v-divider></v-divider>
 
-                    <v-list dense>
-                        <v-list-item>
-                        <v-list-item-content>Produk</v-list-item-content>
-                        <v-list-item-content class="align-end">: {{ item.nama }}</v-list-item-content>
-                        </v-list-item>
-
-                        <v-list-item>
-                        <v-list-item-content>Berat Bersih</v-list-item-content>
-                        <v-list-item-content class="align-end">: {{ item.unit }}</v-list-item-content>
-                        </v-list-item>
-
-                        <v-list-item>
-                        <v-list-item-content>Stok</v-list-item-content>
-                        <v-list-item-content class="align-end">: {{ item.stok }}</v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-
-                    <v-divider></v-divider>
-                </v-card>
+              <v-list dense>
+                <v-img class="white--text align-end" height="200px" v-bind:src="'http://localhost:8081/API_REST/upload/produk/' + item.foto"></v-img>
+                <v-list-item
+                  v-for="(key, index) in filteredKeys"
+                  :key="index"
+                >
+                  <v-list-item-content :class="{ 'blue--text': sortBy === key }">{{ key }}:</v-list-item-content>
+                  <v-list-item-content class="align-end" :class="{ 'blue--text': sortBy === key }">{{ item[key.toLowerCase()] }}</v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
           </v-col>
+        </v-row>
+      </template>
+
+      <template v-slot:footer>
+        <v-row class="mt-2" align="center" justify="center">
+          <span class="grey--text">Items per page</span>
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                dark
+                text
+                color="#B7472A"
+                class="ml-2"
+                v-on="on"
+              >
+                {{ itemsPerPage }}
+                <v-icon>mdi-chevron-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(number, index) in itemsPerPageArray"
+                :key="index"
+                @click="updateItemsPerPage(number)"
+              >
+                <v-list-item-title>{{ number }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+
+          <v-spacer></v-spacer>
+
+          <span
+            class="mr-4
+            grey--text"
+          >
+            Page {{ page }} of {{ numberOfPages }}
+          </span>
+          <v-btn
+            fab
+            dark
+            color="#B7472A"
+            class="mr-1"
+            @click="formerPage"
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            dark
+            color="#B7472A"
+            class="ml-1"
+            @click="nextPage"
+          >
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
         </v-row>
       </template>
     </v-data-iterator>
@@ -42,33 +149,51 @@
 </template>
 
 <script>
-export default {
-        data() {
-            return {
-                dialog: false,
-                keyword: '',
-                shows: [],
-                snackbar: false,
-                search: '',
-                text: '',
-                errors: '',
-                updatedId: '',
-                load: false,
-                request : new FormData,
-                x: null,
-                y: 'top'
-            }
+  export default {
+    data () {
+      return {
+        itemsPerPageArray: [4, 8, 12],
+        search: '',
+        filter: {},
+        sortDesc: false,
+        page: 1,
+        itemsPerPage: 4,
+        sortBy: 'nama',
+        keys: [
+          'Unit',
+          'Stok',
+          'Harga',
+        ],
+        items: [],
+      }
+    },
+    computed: {
+      numberOfPages () {
+        return Math.ceil(this.items.length / this.itemsPerPage)
+      },
+      filteredKeys () {
+        return this.keys.filter(key => key !== `Nama`)
+      },
+    },
+    methods: {
+        getData() {
+            var uri = this.$apiUrl + '/produk'
+            this.$http.get(uri).then(response => {
+                this.items = response.data.data
+            })
         },
-        methods: {
-            getData() {
-                var uri = this.$apiUrl + '/produk'
-                this.$http.get(uri).then(response => {
-                    this.shows = response.data.data
-                })
-            },
-        },
-        mounted() {
-            this.getData();
-        },
-    }
+      nextPage () {
+        if (this.page + 1 <= this.numberOfPages) this.page += 1
+      },
+      formerPage () {
+        if (this.page - 1 >= 1) this.page -= 1
+      },
+      updateItemsPerPage (number) {
+        this.itemsPerPage = number
+      },
+    },
+    mounted() {
+        this.getData();
+    },
+  }
 </script>
